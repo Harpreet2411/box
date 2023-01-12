c5dcb423c2e8f6bac911b0c11361cf8634297696
Pankaj Sharma <rersharma@gmail.com>
2023-01-12 17:23:34 +0530

    Add project files.

---
+++ box/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace box
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j ;
+
+            for(i=0;i<=20; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            for (i = 0; i <= 3; i++)
+            {
+                for (j = 0; j <= 0; j++)
+                {
+
+                    Console.Write("*");
+                }
+                for (j = 0; j <= 0; j++)
+                {
+                    Console.Write("                   ");
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+
+            }
+
+
+
+            Console.Write("    ");
+
+
+            for (i = 0; i <= 12; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();    
+
+          
+            for (i = 0; i <= 3; i++)
+            {
+                for (j = 0; j <= 0; j++)
+                {
+
+                    Console.Write("*");
+                }
+                for (j = 0; j <= 0; j++)
+                {
+                    Console.Write("   ");
+                    Console.Write("*");
+                }
+                for (j = 0; j <= 0; j++)
+                {
+                    Console.Write("           ");
+                    Console.Write("*");
+                }
+                for (j = 0; j <= 0; j++)
+                {
+                    Console.Write("   ");
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+
+
+
+            }
+            Console.Write("    ");
+
+
+            for (i = 0; i <= 12; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            for (i = 0; i <= 3; i++)
+            {
+                for (j = 0; j <= 0; j++)
+                {
+
+                    Console.Write("*");
+                }
+                for (j = 0; j <= 0; j++)
+                {
+                    Console.Write("                   ");
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+
+            }
+            for (i = 0; i <= 20; i++)
+            {
+                Console.Write("*");
+            }
+
+
+
+
+            Console.ReadKey();   
+        }
+
+    }
+}
---
+++ box/divertline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace box
+{
+    internal class divertline
+    {
+        static void Main(string[] args)
+        {
+            int i,j;
+
+            for (i = 0; i <= 5; i++)
+            {
+                Console.Write(" ");
+                for (j = 0; j <= 0; j++)
+                {
+                    Console.Write("*");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
